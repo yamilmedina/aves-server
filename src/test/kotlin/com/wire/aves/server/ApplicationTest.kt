9adb675675c23985096382fa7126f396c1f75621
yamilmedina <yamilmedina@users.noreply.github.com>
2023-07-08 09:50:21 +0200

    chore: layers

--- src/test/kotlin/com/wire/aves/server/ApplicationTest.kt
+++ src/test/kotlin/com/wire/aves/server/ApplicationTest.kt
@@ -1,11 +1,12 @@
-package com.wire
+package com.wire.aves.server
 
 import com.wire.aves.server.application.configureRouting
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.server.testing.*
-import kotlin.test.*
-import io.ktor.http.*
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
